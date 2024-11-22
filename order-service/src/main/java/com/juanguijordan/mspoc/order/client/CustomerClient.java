9cbce750fce492d18c12faae433bc5ef99ce9df0
jguix <juangui@gmail.com>
2024-11-22 16:34:59 +0100

    Small refactor on feign client url config

--- order-service/src/main/java/com/juanguijordan/mspoc/order/client/CustomerClient.java
+++ order-service/src/main/java/com/juanguijordan/mspoc/order/client/CustomerClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.juanguijordan.mspoc.order.config.FeignConfig;
 import com.juanguijordan.mspoc.order.dto.CustomerDTO;
 
-@FeignClient(name = "customer-service", url = "http://localhost:9020/api/customers", configuration = FeignConfig.class)
+@FeignClient(name = "customer-service", url = "${feign.client.customer-service}", configuration = FeignConfig.class)
 public interface CustomerClient {
     @GetMapping("/{id}")
     CustomerDTO getCustomerById(@PathVariable("id") Long id);
