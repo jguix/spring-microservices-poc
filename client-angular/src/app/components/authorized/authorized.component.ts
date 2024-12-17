500c146029685817c2077dd56ede53ca1249b927
jguix <juangui@gmail.com>
2024-12-17 14:40:03 +0100

    Save access token and refresh token to local storage

--- client-angular/src/app/components/authorized/authorized.component.ts
+++ client-angular/src/app/components/authorized/authorized.component.ts
@@ -1,7 +1,8 @@
 // import { TokenService } from './../../services/token.service';
 import { AuthService } from '../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-authorized',
@@ -15,23 +16,23 @@ export class AuthorizedComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
-    // private tokenService: TokenService
+    private tokenService: TokenService,
   ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe( data => {
-      this.code = data.code;
+    // Subscribe to route query params and get "code" param
+    this.activatedRoute.queryParams.subscribe( (params: Params) => {
+      this.code = params.code;
       this.getToken();
     });
   }
 
   getToken(): void {
     this.authService.getToken(this.code).subscribe(
-      data => {
-        console.log(data);
-      //  this.tokenService.setTokens(data.access_token, data.refresh_token);
+      (data: any) => {
+        this.tokenService.setTokens(data.access_token, data.refresh_token);
       },
-      err => {
+      (err: any) => {
         console.log(err);
       }
     );
