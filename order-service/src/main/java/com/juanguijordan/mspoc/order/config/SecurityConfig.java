2d00282e19ec28297ad394c821850438af4c4214
jguix <juangui@gmail.com>
2024-12-17 23:46:48 +0100

    Added CORS configuration to resources

--- order-service/src/main/java/com/juanguijordan/mspoc/order/config/SecurityConfig.java
+++ order-service/src/main/java/com/juanguijordan/mspoc/order/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
         http
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/actuator/**").permitAll() // Exclude actuator endpoints if needed
